18246afaee7249fa3d1e3a61d7828d86bb10f68d
zbzxzm <zhxi2251345299@outlook.com>
2019-06-11 23:21:16 +0800

    feat(add new feat)

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
+from pprint import pprint
+
+from flask_script import Manager, Server
+
 from flask_example import app
-from flask_example.apis.user import user_bp
 from flask_example.apis.upload_files import upload_files_bp
+from flask_example.apis.user import user_bp
+from flask_migrate import Migrate, MigrateCommand
+from flask_example.db.orm import db
+app.register_blueprint(user_bp, url_prefix="/user")
+app.register_blueprint(upload_files_bp, url_prefix="/upload_files")
+
+pprint(app.url_map)
+
+# 迁移
+migrate = Migrate(app, db=db)
+
+
+# 命令
+manager = Manager(app)
+
+manager.add_command("runserver", Server(
+    use_reloader=True,
+    use_debugger=True,
+    host="0.0.0.0",
+    port=8000
+
+))
+
+manager.add_command("db", MigrateCommand)
 
 if __name__ == "__main__":
-    app.register_blueprint(user_bp, url_prefix="/user")
-    app.register_blueprint(upload_files_bp, url_prefix="/upload_files")
-    from pprint import pprint
-    pprint(app.url_map)
-    app.run(host="0.0.0.0", port=8000)
+    # app.run(host="0.0.0.0", port=8000, debug=app.debug)
+    manager.run()
