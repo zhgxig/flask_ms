eee799c473ddad81969c5fbbaffacfbc773601f0
zbzxzm <zhxi2251345299@outlook.com>
2019-06-18 00:40:43 +0800

    feat(add memcache)

--- flask_example/db/memcache.py
+++ flask_example/db/memcache.py
@@ -1,21 +1,28 @@
+import time
+
 from libmc import (
     Client, MC_HASH_MD5, MC_POLL_TIMEOUT, MC_CONNECT_TIMEOUT, MC_RETRY_TIMEOUT
 )
 
 mc = Client(
-    [
-        "localhost",
-        "localhost:11212",
-        "localhost:11212 mc_213"
-    ],
-    do_splt=True,
-    comp_threshold=0,
-    noreply=False,
-    prefix=None,
-    hash_fn=MC_HASH_MD5,
-    failover=False
+    ['127.0.0.1:11211', '127.0.0.1:11212'], do_split=True,
+    comp_threshold=0, noreply=False, prefix=None,
+    hash_fn=MC_HASH_MD5, failover=False
 )
 
-mc.config(MC_POLL_TIMEOUT, 100)
-mc.config(MC_CONNECT_TIMEOUT, 300)
-mc.config(MC_RETRY_TIMEOUT, 5)
+mc.config(MC_POLL_TIMEOUT, 100)  # 100 ms
+mc.config(MC_CONNECT_TIMEOUT, 300)  # 300 ms
+mc.config(MC_RETRY_TIMEOUT, 5)  # 5 s
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+    # res_data = mc.set("today", "hehe", 100)
+    # print(res_data)
+    t0 = time.time()
+    res = mc.get("today")
+    print("res={}".format(res))
+    pprint(dir(mc))
+    # print(mc.config())
+    t1 = time.time()
+    print("cost_time={}".format(t1-t0))
